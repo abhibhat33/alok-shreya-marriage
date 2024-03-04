4d7b81e29eda917505fe15d4e53ff573bb42e8ff
abhishek <abhishek@tikkl.com>
2024-03-04 21:32:21 +0530

    added one photo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import p1 from './p1.jpg';
 
 function App() {
   return (
     <div>
       <header className="App-header">
+      <img src={p1} alt="Alok Shreya Marriage" style={{ maxWidth: '50%' }} />
 
         <p>
           WELCOME TO ALOK - SHREYA MARRIAGE
