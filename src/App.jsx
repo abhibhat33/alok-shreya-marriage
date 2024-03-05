a8d47d619050ae5c5b6c557d51756579affdf840
abhishek <abhishek@tikkl.com>
2024-03-05 13:42:31 +0530

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import the carousel styles
 import './App.css';
 import p1 from './p1.jpg';
-import location from './location.png'
+import location from './location.png';
 
 function App() {
   const images = [p1, p1, p1];
@@ -11,21 +11,32 @@ function App() {
   return (
     <div>
       <header className="App-header">
-        <div  style={{maxWidth: "70%", alignItems: "flex-start"}}>
-      <Carousel autoPlay={true} interval={2000}>
-          {images.map((image, index) => (
-            <div key={index}>
-              <img src={image} alt={`Image ${index}`} style={{ maxWidth: '60%' }}/>
-            </div>
-          ))}
-        </Carousel>
-        <p>
-          WELCOME TO ALOK - SHREYA MARRIAGE
-        </p>
+        <p>WELCOME TO ALOK - SHREYA MARRIAGE</p>
+
+        <div className="carousel-container">
+          <Carousel autoPlay={true} interval={2000}>
+            {images.map((image, index) => (
+              <div key={index} className="carousel-item">
+                <img src={image} alt={`Image ${index}`} />
+              </div>
+            ))}
+          </Carousel>
         </div>
-        <a href="https://goo.gl/maps/LiisgKv448xUEViu6" target="_blank" rel="noopener noreferrer">
-          <img src={location} alt="Location" style={{ maxWidth: '30%', cursor: 'pointer' }} />
-        </a>
+
+        <div className="location-container">
+          <h2>LOCATION</h2>
+          <a
+            href="https://goo.gl/maps/LiisgKv448xUEViu6"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={location}
+              alt="Location"
+              style={{ maxWidth: '30%', height: 'auto', cursor: 'pointer' }}
+            />
+          </a>
+        </div>
       </header>
 
       <section className="gallery-section">
